d7c6361ae883a0557597220b8675a814bf786b1a
fabriciocunhauk <fabriciocunhadeveloper@gmail.com>
2024-03-27 10:02:16 +0000

    PDO first steps

--- section02/index.php
+++ section02/index.php
@@ -1,31 +1,10 @@
 <?php
 require "functions.php";
+// require "router.php";
+require "Database.php";
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+$db = new Database();
 
-// dd($uri);
+$posts = $db->query("select * from posts;")->fetchAll(PDO::FETCH_ASSOC);
 
-$routes = [
-    '/section02/index' => 'controllers/index.php',
-    '/section02/about' => 'controllers/about.php',
-    '/section02/contact' => 'controllers/contact.php',
-];
-
-function routesToController($uri, $routes)
-{
-    if (array_key_exists($uri, $routes)) {
-        require $routes[$uri];
-    } else {
-        abort(404);
-    }
-}
-
-function abort($code = 404)
-{
-    http_response_code($code);
-    require "views/{$code}.php";
-
-    die();
-}
-
-routesToController($uri, $routes);
+dd($posts);
--- section02/router.php
+++ section02/router.php
@@ -1,3 +1,29 @@
 <?php
-require "functions.php";
-require "router.php";
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+// dd($uri);
+
+$routes = [
+    '/section02/index' => 'controllers/index.php',
+    '/section02/about' => 'controllers/about.php',
+    '/section02/contact' => 'controllers/contact.php',
+];
+
+function routesToController($uri, $routes)
+{
+    if (array_key_exists($uri, $routes)) {
+        require $routes[$uri];
+    } else {
+        abort(404);
+    }
+}
+
+function abort($code = 404)
+{
+    http_response_code($code);
+    require "views/{$code}.php";
+
+    die();
+}
+
+routesToController($uri, $routes);
